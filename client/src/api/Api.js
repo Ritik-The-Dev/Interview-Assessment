3de7c68bde06370241fd8bcb2164fc4e1c404951
Ritik-the-dev <joshiritik865@gmail.com>
2026-01-03 09:50:09 +0530

    feat : added loading toast

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -4,18 +4,18 @@ const BaseUrl = import.meta.env.VITE_SERVER_URL;
 
 export const GetAllContactApi = async () => {
   const apiUrl = `${BaseUrl}/api/v1/getAllContacts`;
-  const result = await apiCall("GET", apiUrl);
+  const result = await apiCall("GET", apiUrl, {}, true, "Loading Contacts...");
   return result;
 };
 
 export const AddNewContactApi = async (payload) => {
   const apiUrl = `${BaseUrl}/api/v1/createNewContact`;
-  const result = await apiCall("POST", apiUrl, payload);
+  const result = await apiCall("POST", apiUrl, payload, true, "Creating Contact...");
   return result;
 };
 
 export const DeleteContactApi = async (id) => {
   const apiUrl = `${BaseUrl}/api/v1/deleteContact?id=${id}`;
-  const result = await apiCall("DELETE", apiUrl);
+  const result = await apiCall("DELETE", apiUrl, {}, true, "Deleting Contact...");
   return result;
 };
--- client/src/api/axiosFactory.js
+++ client/src/api/axiosFactory.js
@@ -1,34 +1,47 @@
 import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
 
-export const apiCall = async (method, url, data, showToast = true) => {
+export const apiCall = async (method, url, data, showToast = true, toastData) => {
+  const toastId = toast.loading(toastData || "Loading...");
+
   try {
     const response = await axios({
       method,
       url,
       data
     });
+
+    toast.success(response.data?.message || "Success!", { id: toastId });
+
     return response.data;
   } catch (err) {
     if (err instanceof AxiosError && err.response) {
-      if (
-        err.response.data.success === true ||
-        err.response.data.success === false
-      ) {
-        showToast &&   toast.error(err.response.data.message);
+      const { success, message } = err.response.data;
+
+      if (success === true || success === false) {
+        if (showToast) {
+          toast.error(message || "An error occurred");
+        }
+
         return err.response.data;
       } else {
-        showToast && toast.error("No Response From Server");
+        if (showToast) {
+          toast.error("No Response From Server", { id: toastId });
+        }
+
         return {
           success: false,
-          message: "No Response From Server",
+          message: "No Response From Server"
         };
       }
     } else {
-      showToast && toast.error("No Response From Server");
+      if (showToast) {
+        toast.error("No Response From Server", { id: toastId });
+      }
+
       return {
         success: false,
-        message: "No Response From Server",
+        message: "No Response From Server"
       };
     }
   }
